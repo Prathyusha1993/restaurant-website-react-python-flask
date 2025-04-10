7ef1c761beba7d83f22ea94e87bd3c941c6886f3
prathyusha1993 <prathyusha9310@gmail.com>
2025-04-10 16:39:40 -0700

    added veg, spicy and img_url in database and used flask migration

--- backend_flask_server/main.py
+++ backend_flask_server/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import routes
 
--- backend_flask_server/models.py
+++ backend_flask_server/models.py
@@ -4,9 +4,9 @@ class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    veg = db.Column(db.Boolean, nullable=False)
-    spicy = db.Column(db.Boolean, nullable=False)
-    img_url = db.Column(db.String(200), nullable=False)
+    veg = db.Column(db.Boolean, nullable=False, default=False)
+    spicy = db.Column(db.Boolean, nullable=False, default=False)
+    img_url = db.Column(db.String(200), nullable=False, default='')
     description = db.Column(db.String(200), nullable=True)
     category = db.Column(db.String(50), nullable=False)
 
