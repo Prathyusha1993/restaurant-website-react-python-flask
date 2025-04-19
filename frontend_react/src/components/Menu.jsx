588bd0142b82e3e508308f1d2c5dfc46d4a59590
prathyusha1993 <prathyusha9310@gmail.com>
2025-04-18 17:33:16 -0700

    added all accordion to default open and added redirect to zomato app for ordering

--- frontend_react/src/components/Menu.jsx
+++ frontend_react/src/components/Menu.jsx
@@ -1,134 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Accordion, Button, Badge, Image, ButtonGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Accordion,
+  Button,
+  Badge,
+  Image,
+  ButtonGroup,
+  Modal
+} from "react-bootstrap";
 import { IoMdRadioButtonOn } from "react-icons/io";
 import { FaPepperHot } from "react-icons/fa";
 import Footer from "./Footer";
 import { BASE_URL } from "../App";
+import { SiZomato } from "react-icons/si";
 
-// const menuData = {
-//   Biryani: [
-//     {
-//       name: "Hyderabadi Chicken Biryani",
-//       price: "$12.99",
-//       veg: false,
-//       spicy: true,
-//       img: "/images/biryani1.jpg",
-//     },
-//     {
-//       name: "Veg Dum Biryani",
-//       price: "$10.99",
-//       veg: true,
-//       spicy: false,
-//       img: "/images/biryani2.jpg",
-//     },
-//   ],
-//   Shawarma: [
-//     {
-//       name: "Chicken Shawarma Wrap",
-//       price: "$8.99",
-//       veg: false,
-//       spicy: false,
-//       img: "/images/shawarma1.jpg",
-//     },
-//     {
-//       name: "Paneer Shawarma",
-//       price: "$9.99",
-//       veg: true,
-//       spicy: true,
-//       img: "/images/shawarma2.jpg",
-//     },
-//   ],
-//   Starters: [
-//     {
-//       name: "Chicken Shawarma Wrap",
-//       price: "$8.99",
-//       veg: false,
-//       spicy: false,
-//       img: "/images/shawarma1.jpg",
-//     },
-//     {
-//       name: "Paneer Shawarma",
-//       price: "$9.99",
-//       veg: true,
-//       spicy: true,
-//       img: "/images/shawarma2.jpg",
-//     },
-//   ],
-//   Tandooris: [
-//     {
-//       name: "Chicken Shawarma Wrap",
-//       price: "$8.99",
-//       veg: false,
-//       spicy: false,
-//       img: "/images/shawarma1.jpg",
-//     },
-//     {
-//       name: "Paneer Shawarma",
-//       price: "$9.99",
-//       veg: true,
-//       spicy: true,
-//       img: "/images/shawarma2.jpg",
-//     },
-//   ],
-//   Desserts: [
-//     {
-//       name: "Chicken Shawarma Wrap",
-//       price: "$8.99",
-//       veg: false,
-//       spicy: false,
-//       img: "/images/shawarma1.jpg",
-//     },
-//     {
-//       name: "Paneer Shawarma",
-//       price: "$9.99",
-//       veg: true,
-//       spicy: true,
-//       img: "/images/shawarma2.jpg",
-//     },
-//   ],
-// };
 
 function Menu() {
-    const [selectedCategory, setSelectedCategory] = useState("All");
-    const [menuData, setMenuData] = useState([])
-    const [loading, setLoading] = useState(true)
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [menuData, setMenuData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showOrderModal, setShowOrderModal] = useState(false);
 
-    const getFilteredMenu = () => {
-        if (selectedCategory === 'All') return menuData;
-        return { [selectedCategory]: menuData[selectedCategory]}
-    };
+  const getFilteredMenu = () => {
+    if (selectedCategory === "All") return menuData;
+    return { [selectedCategory]: menuData[selectedCategory] };
+  };
 
-    const groupByCategory = (items) => {
-        return items.reduce((acc, item) => {
-            const category = item.category.charAt(0).toUpperCase() + item.category.slice(1);
-            if (!acc[category]) acc[category] = [];
-            acc[category].push(item);
-            return acc;
-        }, {});
-    };
+  const groupByCategory = (items) => {
+    return items.reduce((acc, item) => {
+      const category =
+        item.category.charAt(0).toUpperCase() + item.category.slice(1);
+      if (!acc[category]) acc[category] = [];
+      acc[category].push(item);
+      return acc;
+    }, {});
+  };
 
-    useEffect(() => {
-        const getMenu = async () => {
-            try{
-                const res = await fetch(BASE_URL+'menu')
-                const data = await res.json()
-                console.log(data)
+  useEffect(() => {
+    const getMenu = async () => {
+      try {
+        const res = await fetch(BASE_URL + "menu");
+        const data = await res.json();
+        console.log(data);
 
-                if (!res.ok){
-                    throw new Error(data.error || 'Somethig went wrong')
-                }
-                const groupedData = groupByCategory(data)
-                setMenuData(groupedData)
-            }
-            catch(err){
-                console.log(err)
-            }
-            finally{
-                setLoading(false)
-            }
+        if (!res.ok) {
+          throw new Error(data.error || "Somethig went wrong");
         }
-        getMenu()
-    }, [])
+        const groupedData = groupByCategory(data);
+        setMenuData(groupedData);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getMenu();
+  }, []);
 
   return (
     <>
@@ -177,58 +106,110 @@ function Menu() {
           </ButtonGroup>
         </div>
 
-        <Accordion defaultActiveKey="0">
-            {Object.entries(getFilteredMenu()).map(([category, items], index) => (
-                <Accordion.Item eventKey={index.toString()} key={category}>
-                <Accordion.Header>{category}</Accordion.Header>
-                <Accordion.Body>
-                  <Row>
-                    {items.map((item, idx) => (
-                        <Col md={6} className="mb-4" key={idx}>
-                            <div className="d-flex">
-                                <Image
-                                    src={item.imgUrl}
-                                    width={80}
-                                    height={80}
-                                    className="me-3 rounded"
-                                    alt={item.name}
-                                    style={{ objectFit: "cover" }}
-                                 />
-                                 <div>
-                                    <h5 className="mb-1">
-                                        {item.name}{" "}
-                                        {item.veg !== null && (item.veg ? 
-                                        (<Badge bg='light' className="ms-1">
-                                            <span style={{border:'1px solid green', padding:'0px 2px 2px 2px'}}>
-                                                <IoMdRadioButtonOn style={{color:'green' }}/>
-                                            </span>
-                                        </Badge>) : (
-                                        <Badge bg='light' className="ms-1">
-                                            <span style={{border:'1px solid red', padding:'0px 2px 2px 2px'}}>
-                                                <IoMdRadioButtonOn style={{color:'red'}}/>
-                                            </span>
-                                        </Badge>
-                                        )
-                                        )}
-                                        {item.spicy && <Badge bg='light' className="ms-2"><FaPepperHot style={{color:'red'}} /></Badge>}
-                                    </h5>
-                                    <p className="mb-2">{item.price}</p>
-                                    <Button size='sm' variant='outline-primary'>Order Now</Button>
-                                 </div>
-                            </div>
-                        </Col>
-                    ))}
-                  </Row>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))}
+        <Accordion defaultActiveKey={['0', '1', '2', '3', '4', '5']}>
+          {Object.entries(getFilteredMenu()).map(([category, items], index) => (
+            <Accordion.Item eventKey={index.toString()} key={category}>
+              <Accordion.Header>{category}</Accordion.Header>
+              <Accordion.Body>
+                <Row>
+                  {items.map((item, idx) => (
+                    <Col md={6} className="mb-4" key={idx}>
+                      <div className="d-flex">
+                        <Image
+                          src={item.imgUrl}
+                          width={80}
+                          height={80}
+                          className="me-3 rounded"
+                          alt={item.name}
+                          style={{ objectFit: "cover" }}
+                        />
+                        <div>
+                          <h5 className="mb-1">
+                            {item.name}{" "}
+                            {item.veg !== null &&
+                              (item.veg ? (
+                                <Badge bg="light" className="ms-1">
+                                  <span
+                                    style={{
+                                      border: "1px solid green",
+                                      padding: "0px 2px 2px 2px",
+                                    }}
+                                  >
+                                    <IoMdRadioButtonOn
+                                      style={{ color: "green" }}
+                                    />
+                                  </span>
+                                </Badge>
+                              ) : (
+                                <Badge bg="light" className="ms-1">
+                                  <span
+                                    style={{
+                                      border: "1px solid red",
+                                      padding: "0px 2px 2px 2px",
+                                    }}
+                                  >
+                                    <IoMdRadioButtonOn
+                                      style={{ color: "red" }}
+                                    />
+                                  </span>
+                                </Badge>
+                              ))}
+                            {item.spicy && (
+                              <Badge bg="light" className="ms-2">
+                                <FaPepperHot style={{ color: "red" }} />
+                              </Badge>
+                            )}
+                          </h5>
+                          <p className="mb-2">{item.price}</p>
+                          <Button
+                            size="sm"
+                            variant="outline-primary"
+                            onClick={() => setShowOrderModal(true)}
+                          >
+                            Order Now
+                          </Button>
+                            <Modal show={showOrderModal} backdrop="true" className="modal-backdrop.show" onHide={() => setShowOrderModal(false)} centered > 
+                              <Modal.Header closeButton>
+                                <Modal.Title>Continue With</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body className='text-center'>
+                                <Button 
+                                variant="outline-primary"
+                                style={{outline:'none', boxShadow:'none', border:'none', padding:0, backgroundColor:'transparent'}}
+                                className="m-2"
+                                href="https://www.zomato.com/bangalore/aha-biriyani-btm-bangalore/order"
+                                target="_blank">
+                                  <SiZomato size={70}  style={{backgroundColor:'red', color:'white', borderRadius:'10px', padding:'5px'}} />
+                                </Button>
+
+                                {/* <Button 
+                                variant="outline-primary"
+                                className="m-2"
+                                href="https://www.zomato.com/bangalore/aha-biriyani-btm-bangalore/order"
+                                target="_blank">
+                                  <SiZomato style={{backgroundColor:'red', color:'white'}} />
+                                </Button> */}
+                              </Modal.Body>
+                            </Modal>
+                        </div>
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
         </Accordion>
         <div className="text-center mt-5">
-            <a href='http://127.0.0.1:5000/static/downloads/menu.pdf' 
+          <a
+            href="http://127.0.0.1:5000/static/downloads/menu.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            download 
-            className="btn btn-outline-secondary">Download Full Menu(PDF)</a>
+            download
+            className="btn btn-outline-secondary"
+          >
+            Download Full Menu(PDF)
+          </a>
         </div>
       </Container>
       <Footer />
